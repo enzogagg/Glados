6bb7ec3fbfbb00bd597d5b8533ebbf987f73d44a
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2025-12-01 15:12:24 +0100

    [MERGE] rebase main into dev

---
+++ documentation/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/src/css/custom.css"),
+];
