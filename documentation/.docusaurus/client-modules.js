d95810a529542b0cb165ef5cafde11da2e037fa1
Laurie <91892061+claurie9@users.noreply.github.com>
2025-12-03 14:42:10 +0100

    [MERGE] Documentation of types and structure of the code.
    * [ADD] first documentation on types and operators. To be imrpoved
    
    * [UPDATE] doc to add css and fix documentation
    
    * [DOC] add doc for test and ci/cd
    
    * [DOC] add doc for test and ci/cd
    
    * [UPDATE] documentation to prevent push of .docusaurus
    
    * [UPDATE] delete documentation/.docusaurus directory
    
    * [ADD] documentation for type
    
    * [ADD] documentation structure of code
    
    * [ADD] sidebar docusaurus
    
    * [FIX] structure du code
    
    * [FIX] types documentation
    
    * [UPDATE] doc logic
    
    ---------
    
    Co-authored-by: Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
    Co-authored-by: Gaggiotti Enzo <146330419+enzogagg@users.noreply.github.com>
    Co-authored-by: Guillaume <guillaume.foutrel@epitech.eu>
    Co-authored-by: yayamgt <yanis.mignot@epitech.eu>

--- documentation/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/enzogaggiotti/Documents/Delivery/Glados/documentation/src/css/custom.css"),
-];
