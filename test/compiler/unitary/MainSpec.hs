a8a776d933f6373a0fd92a08d2f223dbfb762f67
yayamgt <yanis.mignot@epitech.eu>
2025-12-10 19:07:14 +0100

    [UPDATE] adapt new struct

--- test/compiler/unitary/MainSpec.hs
+++ test/compiler/unitary/MainSpec.hs
@@ -3,8 +3,7 @@
 module MainSpec (spec) where
 
 import Test.Hspec
-import ParseArguments (parseContent, getCladExtension, debugParse)
--- import ParseValue (parseValue) -- RETIRÉ (temporairement, car désactivé dans ParseArguments.hs)
+import ParseArguments (parseContent, getCladExtension)
 
 spec :: Spec
 spec = do
@@ -13,28 +12,24 @@ spec = do
         it "returns error when wrong number of arguments" $ do
             let args = ["file1.clad", "file2.clad"]
             result <- parseContent args
-            -- CHANGÉ: Message d'erreur plus précis
             result `shouldBe` Left "wrong number of arguments (expected one .clad file)"
 
         it "returns error when file has invalid extension" $ do
             let args = ["file.txt"]
             result <- parseContent args
-            -- CHANGÉ: Message d'erreur plus précis
             result `shouldBe` Left "invalid file extension (expected .clad)"
 
         it "returns error when file does not exist" $ do
             let args = ["nofile.clad"]
             result <- parseContent args
-            -- CHANGÉ: Utilisation de .clad
             result `shouldBe` Left "file does not exist: nofile.clad"
 
         it "returns error when no input file provided" $ do
             let args = []
             result <- parseContent args
-            -- CHANGÉ: Gère le cas où aucun argument n'est donné (anciennement REPL)
             result `shouldBe` Left "no input file provided"
 
-    describe "getCladExtension" $ do -- CHANGÉ: Renommé
+    describe "getCladExtension" $ do
         it "returns True for .clad files" $ do
             getCladExtension "test.clad" `shouldBe` True
             getCladExtension "path/to/file.clad" `shouldBe` True
@@ -43,7 +38,3 @@ spec = do
             getCladExtension "test.txt" `shouldBe` False
             getCladExtension "test" `shouldBe` False
             getCladExtension "test.scheme" `shouldBe` False
-
-    -- describe "parseValue" $ do
-    --     Suite de tests pour parseValue retirée car parseValue a été désactivé
-    --     ou nécessitera une refonte complète pour la VM (non pertinent pour l'étape de parsing).
