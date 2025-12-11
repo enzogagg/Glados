752692400b940866153f119b528b9fbccf42a0ac
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2025-12-11 14:50:12 +0100

    [FIX] haskell lint in loop.hs

--- vm/src/Execution/Loop.hs
+++ vm/src/Execution/Loop.hs
@@ -15,9 +15,9 @@ execLoop :: VMState -> IO ()
 execLoop state =
     if ip state >= length (instructions state) || ip state < 0
     then putStrLn "Error: Instruction pointer out of bounds"
-    else case instructions state !! (ip state) of
+    else case instructions state !! ip state of
         Halt -> return ()
-        PushConst idx -> nextStep (opPushConst ((constants state) !! idx) state)
+        PushConst idx -> nextStep (opPushConst (constants state !! idx) state)
         PushInt i -> nextStep (opPushInt i state)
         Pop -> nextStep (opPop state)
         Add -> nextStep (opAdd state)
