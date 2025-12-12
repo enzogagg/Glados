b59916162b18ad66e496926dbe8a477869777330
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2025-12-12 15:51:07 +0100

    [REBASE] vm into vm-execution

--- scripts/functional_tests_compiler.sh
+++ scripts/functional_tests_compiler.sh
@@ -4,7 +4,7 @@
 # Ensure we run from the project root
 cd "$(dirname "$0")/.." || exit 1
 
-BINARY="./glados"
+BINARY="./glados-compiler"
 RET=0
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -15,7 +15,7 @@ NC='\033[0m'
 
 setup() {
     echo -e "${BLUE}==> Building project...${NC}"
-    make > /dev/null
+    make compiler > /dev/null
     if [ $? -ne 0 ]; then
         echo -e "${RED}Build failed!${NC}"
         exit 1
@@ -102,59 +102,59 @@ run_basic_tests() {
 
 run_lambda_tests() {
     echo -e "\n${BLUE}--- Lambda Tests ---${NC}"
-    assert_file "Lambda Definition" "test/functional/lambdas/lambda1.scm" 0 "#<procedure>"
-    assert_file "Lambda Application" "test/functional/lambdas/lambda2.scm" 0 "3"
-    assert_file "Lambda Define & Call" "test/functional/lambdas/lambda3.scm" 0 "7"
+    assert_file "Lambda Definition" "test/compiler/functional/lambdas/lambda1.scm" 0 "#<procedure>"
+    assert_file "Lambda Application" "test/compiler/functional/lambdas/lambda2.scm" 0 "3"
+    assert_file "Lambda Define & Call" "test/compiler/functional/lambdas/lambda3.scm" 0 "7"
 }
 
 run_list_tests() {
     echo -e "\n${BLUE}--- List Tests ---${NC}"
-    assert_file "List1" "test/functional/lists/list1.scm" 0 "(1 2 3 4 5)"
-    assert_file "List2" "test/functional/lists/list2.scm" 0 "1"
+    assert_file "List1" "test/compiler/functional/lists/list1.scm" 0 "(1 2 3 4 5)"
+    assert_file "List2" "test/compiler/functional/lists/list2.scm" 0 "1"
 }
 
 run_function_tests() {
     echo -e "\n${BLUE}--- Function Tests ---${NC}"
-    assert_file "Function Definition" "test/functional/function/function1.scm" 0 "7"
+    assert_file "Function Definition" "test/compiler/functional/function/function1.scm" 0 "7"
 }
 
 run_conditional_tests() {
     echo -e "\n${BLUE}--- Conditional Tests ---${NC}"
-    assert_file "If True" "test/functional/conditional/if1.scm" 0 "1"
-    assert_file "If False" "test/functional/conditional/if2.scm" 0 "2"
-    assert_file "If Else" "test/functional/conditional/if3.scm" 0 "21"
+    assert_file "If True" "test/compiler/functional/conditional/if1.scm" 0 "1"
+    assert_file "If False" "test/compiler/functional/conditional/if2.scm" 0 "2"
+    assert_file "If Else" "test/compiler/functional/conditional/if3.scm" 0 "21"
 }
 
 run_builtin_tests() {
     echo -e "\n${BLUE}--- Builtin Tests ---${NC}"
-    assert_file "Addition" "test/functional/builtin/builtins1.scm" 0 "11"
-    assert_file "Equality" "test/functional/builtin/builtins2.scm" 0 "#t"
-    assert_file "Less Than" "test/functional/builtin/builtins3.scm" 0 "#f"
+    assert_file "Addition" "test/compiler/functional/builtin/builtins1.scm" 0 "11"
+    assert_file "Equality" "test/compiler/functional/builtin/builtins2.scm" 0 "#t"
+    assert_file "Less Than" "test/compiler/functional/builtin/builtins3.scm" 0 "#f"
 }
 
 run_general_tests() {
     echo -e "\n${BLUE}--- General Tests ---${NC}"
-    assert_file "Factorial" "test/functional/general/factorial.scm" 0 "#t"
-    assert_file "Superior" "test/functional/general/superior.scm" 0 "3628800"
+    assert_file "Factorial" "test/compiler/functional/general/factorial.scm" 0 "3628800"
+    assert_file "Superior" "test/compiler/functional/general/superior.scm" 0 "#t"
 }
 
 run_float_tests() {
     echo -e "\n${BLUE}--- Float Tests ---${NC}"
-    assert_file "Float1" "test/functional/float/float1.scm" 0 "2.0"
-    assert_file "Float2" "test/functional/float/float2.scm" 0 "0.5"
-    assert_file "Float3" "test/functional/float/float3.scm" 0 "0.3333333333333333"
+    assert_file "Float1" "test/compiler/functional/float/float1.scm" 0 "2.0"
+    assert_file "Float2" "test/compiler/functional/float/float2.scm" 0 "0.5"
+    assert_file "Float3" "test/compiler/functional/float/float3.scm" 0 "0.3333333333333333"
 }
 
 run_string_tests() {
     echo -e "\n${BLUE}--- String Tests ---${NC}"
-    assert_file "String1" "test/functional/string/string1.scm" 0 "Hello, World!"
-    assert_file "String2" "test/functional/string/string2.scm" 0 "This is a test"
-    assert_file "String3" "test/functional/string/string3.scm" 0 "Hello, Glados is the best project ever!"
+    assert_file "String1" "test/compiler/functional/string/string1.scm" 0 "Hello, World!"
+    assert_file "String2" "test/compiler/functional/string/string2.scm" 0 "This is a test"
+    assert_file "String3" "test/compiler/functional/string/string3.scm" 0 "Hello, Glados is the best project ever!"
 }
 
 run_tco_tests() {
     echo -e "\n${BLUE}--- TCO Tests ---${NC}"
-    assert_file "TCO1" "test/functional/tco/tco1.scm" 0 "0"
+    assert_file "TCO1" "test/compiler/functional/tco/tco1.scm" 0 "0"
 }
 
 # --- MAIN ---
