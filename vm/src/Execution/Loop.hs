15df1f141b8e10456a51dd9d40193e735a8def69
Maxime Vaultier <mxm.vaultier@gmail.com>
2025-12-15 11:02:20 +0100

    [ADD] parsing + execution now work together

--- vm/src/Execution/Loop.hs
+++ vm/src/Execution/Loop.hs
@@ -11,10 +11,14 @@ import Execution.State (VMState(..))
 import Types (Instruction(..), Value(..))
 import Execution.Ops (opPushConst, opPushInt, opPop, opAdd, opSub, opEq)
 
+import System.IO (hPutStrLn, stderr)
+
 execLoop :: VMState -> IO ()
 execLoop state =
-    if ip state >= length (instructions state) || ip state < 0
-    then putStrLn "Error: Instruction pointer out of bounds"
+    if ip state == length (instructions state)
+    then return ()
+    else if ip state > length (instructions state) || ip state < 0
+    then hPutStrLn stderr "Error: Instruction pointer out of bounds"
     else case instructions state !! ip state of
         Halt -> return ()
         PushConst idx -> nextStep (opPushConst (constants state !! idx) state)
@@ -24,12 +28,23 @@ execLoop state =
         Sub -> nextStep (opSub state)
         Eq -> nextStep (opEq state)
         Print -> case stack state of
-                    [] -> putStrLn "Error: Print requires a value on the stack"
+                    [] -> hPutStrLn stderr "Error: Print requires a value on the stack"
                     (val : _) -> do
-                        print val
+                        putStrLn (showValue val)
                         nextStep (Right (state {ip = ip state}))
         _ -> nextStep (Left "Error: Unknown instruction")
     where
         nextStep :: Either String VMState -> IO ()
-        nextStep (Left err) = putStrLn ("Runtime Error: " ++ err)
+        nextStep (Left err) = hPutStrLn stderr ("Runtime Error: " ++ err)
         nextStep (Right newState) = execLoop (newState {ip = ip newState + 1})
+
+showValue :: Value -> String
+showValue (IntVal i) = show i
+showValue (FloatVal f) = show f
+showValue (BoolVal b) = if b then "#t" else "#f"
+showValue (CharVal c) = [c]
+showValue (StringVal s) = s
+showValue (ListVal l) = "(" ++ unwords (map showValue l) ++ ")"
+showValue (SymbolVal s) = s
+showValue NilVal = "nil"
+showValue (FunctionVal _) = "<function>"
