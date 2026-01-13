dcde61a96df29712f5303c7d50dd4c5d34966632
yanismgt <yanis.mignot@epitech.eu>
2026-01-13 15:17:14 +0100

    [FIX] error comment

--- compiler/src/optimisation/TailCallOptimization.hs
+++ compiler/src/optimisation/TailCallOptimization.hs
@@ -11,15 +11,14 @@ import Types
 
 optimizeTailCalls :: AST -> AST
 optimizeTailCalls (IAProgram instrs) = IAProgram (map optimizeTailCalls instrs)
-optimizeTailCalls (IAFunctionDef name params ret body) = 
+optimizeTailCalls (IAFunctionDef name params ret body) =
     IAFunctionDef name params ret (markLastInstruction body)
 optimizeTailCalls (IAMain args body) = IAMain args (markLastInstruction body)
 optimizeTailCalls node = node
 
--- Fonction qui cherche la dernière instruction pour y trouver un appel
 markLastInstruction :: [AST] -> [AST]
 markLastInstruction [] = []
 markLastInstruction [IAReturn (IACall name args)] = [IAReturn (IATailCall name args)]
-markLastInstruction [IAIf cond thenB elseB] = 
+markLastInstruction [IAIf cond thenB elseB] =
     [IAIf cond (markLastInstruction thenB) (fmap markLastInstruction elseB)]
 markLastInstruction (x:xs) = x : markLastInstruction xs
